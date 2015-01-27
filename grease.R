2e600bc9ba125c05d6fba17a763c9ea8c5bd3042
Eric Ihli <ericihli@gmail.com>
2015-01-27 15:52:32 -0600

    Initial commit

---
+++ grease.R
@@ -0,0 +1,25 @@
+Batting <- read.csv('lahman/batting.csv')
+Batting$AVG <- Batting$H / Batting$AB
+df <- Batting[,c('yearID', 'playerID', 'HR', 'SB', 'RBI', 'AVG', 'R')]
+df <- subset(df, yearID >=2012)
+df <- replace(df, is.na(df), 0)
+year_averages <- aggregate(df[,3:7], list(df$yearID), mean)
+year2012 <- subset(df, yearID == 2012)
+year2013 <- subset(df, yearID == 2013)
+year2014 <- subset(df, yearID == 2014)
+
+year2012diff <- sweep(year2012[,3:7], 2, as.numeric(year_averages[1,2:6]))
+year2012res <- year2012
+year2012res[,3:7] <- year2012diff
+
+year2013diff <- sweep(year2013[,3:7], 2, as.numeric(year_averages[1,2:6]))
+year2013res <- year2013
+year2013res[,3:7] <- year2013diff
+
+year2014diff <- sweep(year2014[,3:7], 2, as.numeric(year_averages[1,2:6]))
+year2014res <- year2014
+year2014res[,3:7] <- year2014diff
+
+allyears <- rbind(year2012res, year2013res)
+allyears <- rbind(allyears, year2014res)
+
